07123a7edb1caac082d35fbe1d8954b2d42b2852
hatemjaber <hatem@7079.net>
2020-02-15 13:25:10 -0500

    added server.js for api; test route working

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+
+// .env file to use for this instance
+require("dotenv").config();
+
+// default port number 3500 if we don't have an .env file
+const PORT = process.env.PORT || 3500;
+
+// create express app
+const app = express();
+
+// body parser parses json and non-json requests extracting params...
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("It's working...");
+});
+
+app.listen(PORT, () => console.log(`JSON app is running on port::${PORT}`));
